1f215a63e1eb74023c42722de5b78a05349c768e
Manan Goel <manan.goel@research.iiit.ac.in>
2018-04-05 09:50:10 +0530

    Functioning LogIn

--- signin.py
+++ signin.py
@@ -22,16 +22,41 @@ def __init__(self, email, username, password, DOB):
    self.DOB = DOB
 
 @app.route('/', methods = ['GET', 'POST'])
-def sign_up():
-    print(request.method)
+def sign_up(message=None):
     if request.method == 'POST':
-        if not request.form['email'] or not request.form['username'] or not request.form['password'] or not request.form['DOB']:
-            flash('Please enter all the fields', 'error')
-        else:
-            user = users(email = request.form['email'], username = request.form['username'], password = request.form['password'], DOB = request.form['DOB'])
-            db.session.add(user)
-            db.session.commit()
-    return render_template('home.html')
+        if 'SignUp' in request.form:
+            print(request.form)
+            if not request.form['email'] or not request.form['username'] or not request.form['password'] or not request.form['DOB']:
+                flash('Please enter all the fields', 'error')
+            else:
+                x = users.query.filter_by(username=request.form['username']).first()
+                print(x)
+                if x is None:
+                    user = users(email = request.form['email'], username = request.form['username'], password = request.form['password'], DOB = request.form['DOB'])
+                    db.session.add(user)
+                    db.session.commit()
+                    #print("HAHA")
+                else:
+                    message = 'Username already taken'
+
+        elif 'SignIn' in request.form:
+            print(request.form)
+            if request.form['username'] is '':
+                message = "Please Enter Username"
+            elif not request.form['password']:
+                message = "Please Enter Password"
+            else : 
+                x = users.query.filter_by(username=request.form['username']).first()
+                if x is None:
+                    message = "Incorrect Username"
+                elif request.form['password'] is None:
+                    message = 'Please Enter Password'            
+                elif str(x.password) == str(request.form['password']) and x is not None:
+                    print("Yay! Logged In")
+                elif str(x.password) != str(request.form['password']):
+                    message = "Incorrect Password"
+                
+    return render_template('home.html', message=message)
 
 if __name__ == '__main__':
     db.create_all()
