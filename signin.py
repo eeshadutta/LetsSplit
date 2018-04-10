7409d7511867e86f1b16973346d85434d71c89cf
Eesha Dutta <eeshadutta99@gmail.com>
2018-04-10 23:12:56 +0530

    Add Friend implemented

--- signin.py
+++ signin.py
@@ -37,7 +37,7 @@ class users(DB.Model):
 class friends(DB.Model):
     id = DB.Column('user_id', DB.Integer, primary_key = True)
     username = DB.Column(DB.String(50))
-    friends = DB.Column(DB.String(8000))
+    friend = DB.Column(DB.String(8000))
     
     def __init__(self, username, friends=''):
         self.username = username
@@ -57,7 +57,6 @@ def sign_up(message=None):
                 if x is None:
                     img = request.files['profile_pic']
                     img.save(secure_filename(img.filename))
-                    #os.makedirs("./profile_pics", exist_ok=True)
                     os.system("mv " + secure_filename(img.filename) + " ./static/")
                     user = users(email = request.form['email'], username = request.form['username'], password = request.form['password'], DOB = request.form['DOB'], profile_pic_url = secure_filename(img.filename), name = request.form['name'], bio = request.form['bio'])
                     user_friends = friends(username=request.form['username'])
@@ -90,7 +89,14 @@ def sign_up(message=None):
 @app.route('/<username>/search/<query>', methods=['GET', 'POST'])
 def search_results(username, query, message=None):
     url_dict = {}
+    friend_list = []
+    fr1 = friends.query.filter_by(username=username).first()
+    friend_list = fr1.friend.split(',')
     results = users.query.filter(users.username.startswith(query)).all()
+    for res in results:
+        if res.username == username:
+            results.remove(res)
+            break
     for x in results:
         url_dict[x.username] = x.profile_pic
     if not results:
@@ -100,14 +106,16 @@ def search_results(username, query, message=None):
             return redirect(url_for('search_results', username=username, query=request.form['search_name']))
         if 'logout' in request.form:
             return redirect(url_for('sign_up'))
-        if 'add_friends' in request.form:
-            x = friends.query.filter_by(username=username).first()
-            print(request.form['friend_to_be_added'])
-            z = x.friend
-            x.friend = z + request.form['friend_to_be_added'] + ','
+        if 'add_friend' in request.form:
+            fr2 = friends.query.filter_by(username=request.form['friend_to_be_added']).first()
+            a = fr2.friend
+            z = fr1.friend
+            fr1.friend = z + request.form['friend_to_be_added'] + ','
+            fr2.friend = a + username + ','
+            friend_list = fr1.friend.split(',')
             DB.session.commit()
 
-    return render_template('search_results.html', username=username, results=results, message=message, profile_pic_dict=url_dict)
+    return render_template('search_results.html', username=username, results=results, message=message, profile_pic_dict=url_dict, friend_list=friend_list)
 
 
 @app.route('/<username>', methods=['GET', 'POST'])
